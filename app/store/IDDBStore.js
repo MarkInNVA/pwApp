731f4cf369ebfbe0f6dd7a5e6b4879fd892c713a
Mark Reidy <markinnva@gmail.com>
2014-05-12 11:09:00 -0400

    wip...

---
+++ app/store/IDDBStore.js
@@ -0,0 +1,27 @@
+Ext.define('PWApp.store.IDDBStore', {
+    extend: 'Ext.data.Store',
+    storeId: 'IDDBStore',
+
+    fields: [ 'name' ],
+    data: [
+        {name: 'ANTRIM' },
+        {name: 'APPALACHIAN' },
+        {name: 'CBM' },
+        {name: 'CIMAREX' },
+        {name: 'FERRON' },
+        {name: 'INDIANA' },
+        {name: 'MISSISSIPPI' },
+        {name: 'MONTANACBM' },
+        {name: 'NATCARB' },
+        {name: 'NORTHDAKOTA' },
+        {name: 'OHBRINE' },
+        {name: 'PALODURO' },
+        {name: 'PASHIN' },
+        {name: 'POWDERRIVERCBM' },
+        {name: 'ROCKIES' },
+        {name: 'USGSARK' },
+        {name: 'USGSMAIN' },
+        {name: 'USGSOK' },
+        {name: 'WYOGCC' }
+    ]    
+});
